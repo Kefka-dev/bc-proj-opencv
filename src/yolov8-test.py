46620024fb5dd87a23d24fce4b6bcbbbc44c519f
Kefka <sith.patri@gmail.com>
2025-03-04 22:46:05 +0100

    splits mixed folder into two folders which are compatible with yolo training

--- src/yolov8-test.py
+++ src/yolov8-test.py
@@ -4,11 +4,18 @@ from ultralytics import YOLO
 import math
 import torch
 
-video_path = "testvideos/main/ch01_20241022100000.mp4"
-video_path2 = "testvideos/main/ch03_20241022105505.mp4"
-cap = cv2.VideoCapture(video_path2)
+video_path = "../testvideos/ch01_20241022100000.mp4"
+video_path2 = "../testvideos/ch03_20241022105505.mp4"
+video_path3 = "../testvideos/ch02_20241022130405.mp4"
+video_path4 = "../testvideos/ch15_20241022105615.mp4"
+video_path5 = "../testvideos/ch14_20241022100000.mp4"
+cap = cv2.VideoCapture(video_path5)
 
-MODEL = "yolov8x.pt"
+# MODEL = "yolov8x.pt"
+#model trained in 10 epochs
+MODEL = "E:\\Skola\\FEI\\bakalarka\\bc-proj-opencv\\src\\finetunning\\runs\\detect\\train3\\weights\\best.pt"
+#model trained in 100 epochs
+MODEL2= "E:\\Skola\\FEI\\bakalarka\\bc-proj-opencv\\src\\finetunning\\runs\\detect\\train\\weights\\best.pt"
 model = YOLO(MODEL)  # Load the model normally (WITHOUT classes=0 here)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -29,27 +36,27 @@ while True:
         break
 
     results = model.predict(frame, classes=0)  # Predict without class filtering
-    annotated_frame = results[0].plot()
-    # for r in results:
-    #     boxes = r.boxes
-    #     names = r.names # Get class names
-    #
-    #     for box in boxes:
-    #         x1, y1, x2, y2 = box.xyxy[0]
-    #         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-    #         cls = int(box.cls[0]) # Get class index
-    #         class_name = names[cls] # Get class name
-    #
-    #         cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-    #
-    #         confidence = math.ceil((box.conf[0] * 100)) / 100
-    #         confidence_text = f"{class_name} {confidence:.1f}"  # Include class name
-    #
-    #         text_offset_y = -5
-    #
-    #         cv2.putText(frame, confidence_text, (x1, y1 + text_offset_y), font, font_scale, text_color, font_thickness) # Display class name and confidence
+    # annotated_frame = results[0].plot()
+    for r in results:
+        boxes = r.boxes
+        names = r.names # Get class names
 
-    cv2.imshow('frame', annotated_frame)
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cls = int(box.cls[0]) # Get class index
+            class_name = names[cls] # Get class name
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+            confidence_text = f"{class_name} {confidence:.1f}"  # Include class name
+
+            text_offset_y = -5
+
+            cv2.putText(frame, confidence_text, (x1, y1 + text_offset_y), font, font_scale, text_color, font_thickness) # Display class name and confidence
+
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 

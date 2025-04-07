d3812a5e9c6cf6ec0a791f914799ddcc4ab90496
Kefka <sith.patri@gmail.com>
2025-04-07 08:12:44 +0200

    finetunening

--- src/finetunning/finetune.py
+++ src/finetunning/finetune.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO("yolo11n.pt")
-    results = model.train(data="E:\\Skola\\FEI\\bakalarka\\bc-proj-opencv\\dataset\\data.yaml", epochs=100, imgsz=640, device=0, plots=True)
+    model = YOLO("yolo11m.pt")
+    results = model.train(data="E:\\Skola\\FEI\\bakalarka\\bc-proj-opencv\\merged_dataset\\data.yaml", epochs=100, imgsz=640, device=0, plots=True)
